a48f7d149a8eb8cb1e8e6b50250619bf6e78aa5d
armaniv <valearmani95@gmail.com>
2017-12-26 15:50:59 +0100

    Refactor

--- graph-parser_c/src/test_armani/test.py
+++ graph-parser_c/src/test_armani/test.py
@@ -6,10 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import collections as col
-
 import warnings
-warnings.filterwarnings("ignore")
 
+warnings.filterwarnings("ignore")
 
 
 def ReadGrafoDaJson(url):
@@ -18,47 +17,44 @@ def ReadGrafoDaJson(url):
     return grafo
 
 
-
-def PrintGrafoInJson(G,njproto='OLSR',njversion='0.1',njrevision='a09z',njmetric='ETX'):
-    js= to_netjson(njproto,
+def PrintGrafoInJson(G, njproto='OLSR', njversion='0.1', njrevision='a09z',
+                     njmetric='ETX'):
+    js = to_netjson(njproto,
                     njversion,
                     njrevision,
                     njmetric,
                     G.nodes(data=True),
                     G.edges(data=True), dict=True)
-    f1=open('./topology.json', 'w+')
+    f1 = open('./topology.json', 'w+')
     f1.write(json.dumps(js, indent=4))
     f1.close()
 
 
-
-def GeneraGrafoCaveman(num,size,randWeight=False):
+def GeneraGrafoCaveman(num, size, randWeight=False):
     G = nx.connected_caveman_graph(num, size)
-    for u,v,d in G.edges(data=True):
-        if(randWeight==False):
-            d['weight']=1
+    for u, v, d in G.edges(data=True):
+        if(randWeight == False):
+            d['weight'] = 1
         else:
-            d['weight']=np.random.uniform(1.0,1.1)
+            d['weight'] = np.random.uniform(1.0, 1.1)
     return G
-
 
 
 #   p=b*exp(-d/aL)
-def GeneraGrafoWaxman(num,b=0.4,a=0.1,randWeight=False):
-    G = nx.waxman_graph(num,beta=b,alpha=a)
-    for u,v,d in G.edges(data=True):
-        if(randWeight==False):
-            d['weight']=1
+def GeneraGrafoWaxman(num, b=0.4, a=0.1, randWeight=False):
+    G = nx.waxman_graph(num, beta=b, alpha=a)
+    for u, v, d in G.edges(data=True):
+        if(randWeight == False):
+            d['weight'] = 1
         else:
-            d['weight']=np.random.uniform(1.0,1.1)
+            d['weight'] = np.random.uniform(1.0, 1.1)
     return G
 
 
-
-def  BrandesCutPoint(g):
-    #per compatibilita' con libreria C, rimuvo eventali nodi sconnessi 
+def BrandesCutPoint(g):
+    # per compatibilita' con libreria C, rimuvo eventali nodi sconnessi
     g.remove_nodes_from(nx.isolates(g))
-    
+
     n_nodes = len(g.nodes())
     ap = [x for x in nx.articulation_points(g)]
     bconn = list(nx.biconnected_component_subgraphs(g))
@@ -69,37 +65,37 @@ def  BrandesCutPoint(g):
         cp_dict[p] = [b for b in bconn if p in b]
 
     bc = col.defaultdict(int)
-    
+
     for p, bconns in cp_dict.items():
         for bcs in bconns:
-            h=nx.Graph(g)
+            h = nx.Graph(g)
             for altrebcs in bconns:
-                if(altrebcs!=bcs):
+                if(altrebcs != bcs):
                     rimuovi = altrebcs.nodes()
                     rimuovi.remove(p)
                     h.remove_nodes_from(rimuovi)
-            b = nx.betweenness_centrality(h, weight='weight',endpoints=False,normalized=False)[p]
+            b = nx.betweenness_centrality(h, weight='weight', endpoints=False,
+                                          normalized=False)[p]
             bc[p] += b
         bc[p] += (n_nodes - 1)
-    for n, b in nx.betweenness_centrality(g, weight='weight',endpoints=True,normalized=False).iteritems():
+    for n, b in nx.betweenness_centrality(g, weight='weight', endpoints=True,
+                                          normalized=False).iteritems():
         if n not in ap:
             bc[n] = b
     return bc
 
 
-
-def PrintDizionarioToCsv(diz,nomefile):
+def PrintDizionarioToCsv(diz, nomefile):
     with open(nomefile, "w") as f:
         for k, v in diz.iteritems():
             print >>f, k, v
 
 
-
 def GeneraNuovoGrafo(tipo):
-    if(tipo==0):
-        graph= GeneraGrafoCaveman(2,3,True)
+    if(tipo == 0):
+        graph = GeneraGrafoCaveman(2, 3, True)
     else:
-        graph= GeneraGrafoWaxman(12,0.47,0.47,True)
+        graph = GeneraGrafoWaxman(12, 0.47, 0.47, True)
 
     PrintGrafoInJson(graph)
     nx.draw(graph, with_labels=True)
@@ -107,11 +103,11 @@ def GeneraNuovoGrafo(tipo):
 
 
 def RunTest():
-    graph= ReadGrafoDaJson('topology.json')
-    
+    graph = ReadGrafoDaJson('topology.json')
+
     BC_Cut = BrandesCutPoint(graph)
 
-    PrintDizionarioToCsv(BC_Cut,"Cut_point.dat")
+    PrintDizionarioToCsv(BC_Cut, "Cut_point.dat")
 
     os.system("sh C_scr.sh")
     os.system("gnuplot gnup_scr.sh")
@@ -123,5 +119,5 @@ nx.draw(graph, with_labels=True)
 plt.show()
 '''
 
-#GeneraNuovoGrafo(1)
+# GeneraNuovoGrafo(1)
 RunTest()
